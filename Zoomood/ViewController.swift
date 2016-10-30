2309e41f46a8b8f1366c0e9610a8e75ee0460087
Ulrich Lehner <me@ulrichlehner.com>
2016-10-30 16:56:25 +0100

    Initial Commit

---
+++ Zoomood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Zoomood
+//
+//  Created by Ulrich Lehner on 30/10/2016.
+//  Copyright © 2016 Ulrich Lehner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
